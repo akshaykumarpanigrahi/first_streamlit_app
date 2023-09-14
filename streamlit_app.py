dd3cdf0f68990de268ae11a344251ba8a603081a
akshaykumarpanigrahi <71829532+akshaykumarpanigrahi@users.noreply.github.com>
2023-09-14 13:11:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ streamlit.title('My Parents new healthy dinner')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
